23381ea87254bb3ff5e339b55c70a08f9919ac56
Virtike <ride.isaak@gmail.com>
2019-04-08 11:57:09 +1000

    Update firstrun.sh with correct config path

--- h5ai/firstrun.sh
+++ h5ai/firstrun.sh
@@ -5,5 +5,5 @@ if [ -f /config/options.json ]; then
   echo "Using existing config file."
 else
   echo "Creating config from template."
-  mv  /usr/share/h5ai/_h5ai/conf/options.json.bak /config/options.json
+  mv  /usr/share/h5ai/_h5ai/private/conf/options.json.bak /config/options.json
 fi
